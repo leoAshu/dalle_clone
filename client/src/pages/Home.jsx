28de0a8caf75548f95bbb2d91ba558001c68f583
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-01 17:14:53 +0530

    implements search functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,8 +13,10 @@ const RenderCards = ({ data, title }) => {
 
 const Home = () => {
     const [loading, setLoading] = useState(false);
-    const [searchText, setSearchText] = useState('');
     const [allPosts, setAllPosts] = useState(null);
+    const [searchText, setSearchText] = useState('');
+    const [searchedResults, setSearchedResults] = useState(null);
+    const [searchTimeout, setSearchTimeout] = useState(null);
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -44,6 +46,18 @@ const Home = () => {
         fetchPosts();
     }, []);
 
+    const handleSearchChange = (e) => {
+        clearTimeout(searchTimeout);
+
+        setSearchText(e.target.value);
+
+        setSearchTimeout(setTimeout(() => {
+            const searchResults = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()))
+            
+            setSearchedResults(searchResults);
+        }, 500));
+    }
+
     return (
         <section className='max-w-7xl mx-auto'>
             <div>
@@ -52,7 +66,14 @@ const Home = () => {
             </div>
 
             <div className='mt-16'>
-                <FormField />
+                <FormField 
+                    labelName='Search posts'
+                    type='text'
+                    name='text'
+                    placeholder='Search posts'
+                    value={searchText}
+                    handleChange={handleSearchChange}
+                />
             </div>
 
             <div className='mt-10'>
@@ -70,7 +91,7 @@ const Home = () => {
                         <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
                             {searchText ? (
                                 <RenderCards 
-                                    data={[]}
+                                    data={searchedResults}
                                     title='No search results found'
                                 />
                             ) : (
