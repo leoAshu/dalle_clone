ede87f14cc1e25dc3a7828bbde03c19f7464d07c
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-01 18:18:53 +0530

    resets last commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors'
 
-import connectDB from './mongodb/connect.mjs';
+import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
